d8ab6e4d3df99ac0db287e632e60a8286e137d38
donny_444 <104888835+donny444@users.noreply.github.com>
2022-10-04 22:34:48 +0700

    Add files via upload

--- for_loop_test.py
+++ for_loop_test.py
@@ -1,11 +1,14 @@
 i = 0
-limit = 11
-for i in range(10):
-    i += 1
-    if i%2 != 0:
-        print(str(i) + " is Odd Number")
-    elif i%2 == 0:
-        print(str(i) + " is Even Number")
-    else:
-        break
+n = int(input("Enter Natural Number: "))
+if n>0:
+    for i in range(n):
+        i += 1
+        if i%2 != 0:
+            print(str(i) + " is Odd Number")
+        elif i%2 == 0:
+            print(str(i) + " is Even Number")
+        else:
+            break
+else:
+    print("Only Natural Number")
 print("Finished")
