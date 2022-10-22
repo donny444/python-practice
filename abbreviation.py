841534da0e31f10b0b5cdb26ceff64a1e2f65a84
donny_444 <104888835+donny444@users.noreply.github.com>
2022-10-22 20:05:48 +0700

    Add files via upload

---
+++ abbreviation.py
@@ -0,0 +1,9 @@
+abbreviation = {
+    "AFK": "Away From Keyboard",
+    "BRB": "Be Right Back",
+    "FYI": "For Your Information",
+    "IKR": "I Know, Right?",
+    "WFH": "Work From Home",
+    "WYATB": "Wish You All The Best"
+}
+print(abbreviation.get("WYATB","I don't know"))
