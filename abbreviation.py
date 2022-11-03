28b7b2c508c38607be0a31606ce22ec9e5f90da6
donny_444 <104888835+donny444@users.noreply.github.com>
2022-11-03 21:58:13 +0700

    Add files via upload

--- abbreviation.py
+++ abbreviation.py
@@ -1,9 +1,12 @@
 abbreviation = {
     "AFK": "Away From Keyboard",
     "BRB": "Be Right Back",
+    "BTW": "By The Way",
     "FYI": "For Your Information",
     "IKR": "I Know, Right?",
+    "LOL": "Laughing  Out Loud",
+    "NSFW": "Not Safe For Work",
     "WFH": "Work From Home",
     "WYATB": "Wish You All The Best"
 }
-print(abbreviation.get("WYATB","I don't know"))
+print(abbreviation.get("WY",'"IDK" means "I Don\'t Know"'))
