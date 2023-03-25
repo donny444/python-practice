4af84c883e021bc4b2de9c20e3655c41b07201b3
donny_444 <104888835+donny444@users.noreply.github.com>
2023-03-25 23:16:27 +0700

    Add files via upload

---
+++ pandas/pandas_3.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+books = pd.DataFrame(
+    {
+        "Title": [
+            "Overengineering",
+            "Reverse Engineering",
+            "Can't Shower"
+        ],
+        "Pages": [300, 500, 50],
+        "HaveEbook": [False, False, True]
+    }
+)
+#print()
