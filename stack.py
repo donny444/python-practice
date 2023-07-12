c749a78fdc4bb94c68134cd634c7a0ed33f15041
donny_444 <104888835+donny444@users.noreply.github.com>
2023-07-12 21:44:30 +0700

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,18 @@
+sampleArray = [17.99, "donny", False, 8];
+
+#Last In
+sampleArray.append("homie");
+print(sampleArray);
+
+sampleArray.append("neighbor");
+sampleArray.append("family");
+print(sampleArray);
+
+#Last Out
+sampleArray.pop();
+print(sampleArray);
+
+sampleArray.pop();
+sampleArray.pop();
+sampleArray.pop();
+print(sampleArray);
