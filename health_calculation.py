3a793b218e435683a1bd8f946e38cb8335412641
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-18 21:12:36 +0700

    improve algorithm

--- health_calculation.py
+++ health_calculation.py
@@ -6,35 +6,27 @@ sex = str(input("Enter your sex [Male/Female]: "))
 workout = str(input("How frequently do you workout? :\na)rarely or never\nb)1-3 times per week\nc)4-5 times per week\nd)6-7 times per week\ne)up to 2 times per day\n"))
 def bmi(w, h):
     print("BMI Calculating...")
-    bmiVal = float(w/((h*h)*(0.01*0.01)))
-    return "Your BMI is : " + str(bmiVal)
+    return float(w/((h*h)*(0.01*0.01)))
 def bmr(w, h, sex, age):
-    global bmrVal;
     print("BMR Calculating...")
     if sex == "Male":
-        bmrVal = float(66+(13.7*w)+(5*h)-(6.8*age))
+        return float(66+(13.7*w)+(5*h)-(6.8*age))
     elif sex == "Female":
-        bmrVal = float(665+(9.6*w)+(1.8*h)-(4.7*age))
-    return "Your BMR is : " + str(bmrVal)
-def tdee(bmrVal):
+        return float(665+(9.6*w)+(1.8*h)-(4.7*age))
+def tdee():
     print("TDEE Calculating...")
     if workout == "a":
-        tdeeVal = 1.2*bmrVal
-        return "Your TDEE is " + str(tdeeVal)
+        return 1.2*bmr(w, h, sex, age)
     elif workout == "b":
-        tdeeVal = 1.375*bmrVal
-        return "Your TDEE is " + str(tdeeVal)
+        return 1.375*bmr(w, h, sex, age)
     elif workout == "c":
-        tdeeVal = 1.55*bmrVal
-        return "Your TDEE is " + str(tdeeVal)
+        return 1.55*bmr(w, h, sex, age)
     elif workout == "d":
-        tdeeVal = 1.7*bmrVal
-        return "Your TDEE is " + str(tdeeVal)
+        return 1.7*bmr(w, h, sex, age)
     elif workout == "e":
-        tdeeVal = 1.9*bmrVal
-        return "Your TDEE is " + str(tdeeVal)
-print(bmi(w, h))
-print(bmr(w, h, sex, age))
-print(tdee(bmrVal))
+        return 1.9*bmr(w, h, sex, age)
+print("Your BMI Value is " + str(bmi(w, h)))
+print("Your BMR Value is " + str(bmr(w, h, sex, age)))
+print("Your TDEE Value is " + str(tdee()))
 
 #Next time: use try except
