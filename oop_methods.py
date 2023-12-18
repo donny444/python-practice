2f2ff39f64cada3c1bc18fee938128592ab72ea1
donny_444 <104888835+donny444@users.noreply.github.com>
2023-12-18 12:52:09 +0700

    Add files via upload

--- oop_methods.py
+++ oop_methods.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Dessert:
     def __init__(self, name):
         self.name = name
@@ -19,6 +21,19 @@ class Health:
     def staticBmi(weight, height):
         return weight / ((height**2)/10000)
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    @classmethod
+    def fromBirthYear(cls, name, year):
+        return cls(name, date.today().year - year)
+    
+    @staticmethod
+    def isAdult(age):
+        return age > 18
+
 dessert1 = Dessert("Chocolate")
 dessert2 = Dessert("Lollipop")
 dessert3 = Dessert("Rotee")
@@ -28,3 +43,11 @@ health1 = Health(80, 180)
 print(dessert1.display())
 print(health1.classBmi())
 print(health1.staticBmi(80, 180))
+
+person1 = Person("Donny", 19)
+person2 = Person("Donny", 2004)
+
+print(person1.age)
+print(person2.age)
+
+print(f"Donny is an adult?: {Person.isAdult(19)}")
